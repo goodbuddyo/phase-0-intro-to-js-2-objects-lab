102b2240fc6e5af4cc7cec55654fed6862135792
Seano <sean01234@yahoo.com>
2023-04-01 00:42:23 -0400

    second commit, deleted previous fork

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+// Write your solution in this file!
+
+const employee = {
+  name: "Tom",
+  streetAddress: "5 Cherry St., Anytown, NY"
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newObj = { ...obj };
+  newObj[key] = value;
+  return newObj;
+}
+
+// console.log(updateEmployeeWithKeyAndValue(employee, "name", "Steve"))
+// { name: 'Steve', streetAddress: '5 Cherry St., Anytown, NY' }
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Bill"))
+// { name: 'Bill', streetAddress: '5 Cherry St., Anytown, NY' }
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+// console.log(deleteFromEmployeeByKey(employee, "name"))
+// { streetAddress: '5 Cherry St., Anytown, NY' }
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
+
+// console.log(destructivelyDeleteFromEmployeeByKey(employee, "name"))
+// { streetAddress: '5 Cherry St., Anytown, NY' }
+
+
